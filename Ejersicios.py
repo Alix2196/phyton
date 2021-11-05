ff1300e0709db2ed49d23bdf9ac1a541b0308589
Alix <angelica962008@hotmail.com>
2021-11-06 01:29:51 +0700

    Este es mi segundo proyecto subido

---
+++ Ejersicios.py
@@ -0,0 +1,26 @@
+def mensaje ():
+
+
+
+    print("Estamos aprendiendo python")
+    print ("Estamos aprendiendo instrucciones basicas")
+    print("Poco a poco iremos avanzando")
+
+
+mensaje()
+
+mensaje()
+print("Ejecutando coddigo fuera de funcionx")
+mensaje()
+
+saludo = "¡hola mundo!"
+print (saludo)
+
+def suma(num1,num2)
+
+    print(num1+num2)
+    suma (5,7)
+    suma(2,3)
+    suma(35,358)
+
+
